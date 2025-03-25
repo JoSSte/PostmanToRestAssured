6f490c2a197c33b066c33ad3bf2caa93b33e8f8c
Jonas Stumph Stevnsvig <jonas@stevnsvig.com>
2025-03-25 22:11:17 +0100

    Paremeterized testing and added testcases

--- src/test/java/dk/kodeninjaer/testing/converter/PatternsTest.java
+++ src/test/java/dk/kodeninjaer/testing/converter/PatternsTest.java
@@ -1,19 +1,34 @@
 package dk.kodeninjaer.testing.converter;
 
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
-import org.junit.jupiter.api.io.TempDir;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.stream.Stream;
 
 public class PatternsTest {
+    /*
+     * 
+     */
+    static final String[] parameters = {
+        "some other script",
+        "pm.environment.get(\"someValue\");",
+        "pm.environment.set(\"someKey\",\"someValue\");",
+        "pm.environment.set(\"someKey\",someReturnValue.method());",
+        "console.log(\"someValue\");",
+        "alert(\"someValue\");",
+        "pm.globals.set(\"someKey\",\"someValue\");",
+        "pm.globals.get(\"someValue\");",
+        "pm.globals.set(\"someKey\",someReturnValue.method());"
+    };
+    
+
+    @DisplayName("Test Class cannot be instantiated")
     @Test
     public void testConstructor() {
         // Verify we can't instantiate the class
@@ -22,31 +37,57 @@ public class PatternsTest {
         });
     }
     
-    @Test
-    public void testEnvironmentSet() {
-        String script = "pm.environment.set(\"key\", \"value\");";
+    @DisplayName("Test Environment Set")
+    @ParameterizedTest(name = "Test {index} with script: {arguments}")
+    @MethodSource("provideSetParameters")
+    public void testEnvironmentSet(String script, boolean expected) {
         Matcher matcher = Patterns.ENVIRONMENT_SET.matcher(script);
-        assertTrue(matcher.find());
+        assertEquals(expected, matcher.find());
     }
 
-    @Test
-    public void testEnvironmentSetNegative() {
-        String script = "some other script";
-        Matcher matcher = Patterns.ENVIRONMENT_SET.matcher(script);
-        assertFalse(matcher.find());
-    }
-    
-    @Test
-    public void testEnvironmentGet() {
-        String script = "pm.environment.get(\"someValue\");";
-        Matcher matcher = Patterns.ENVIRONMENT_GET.matcher(script);
-        assertTrue(matcher.find());
+    /**
+     * Method to provide the parameters for the testEnvironmentSet method
+     * @return
+     */
+    private static Stream<Arguments> provideSetParameters() {
+        return Stream.of(
+                Arguments.of(parameters[0], false),
+                Arguments.of(parameters[1], false),
+                Arguments.of(parameters[2], true),
+                Arguments.of(parameters[3], true),
+                Arguments.of(parameters[4], false),
+                Arguments.of(parameters[5], false),
+                Arguments.of(parameters[6], false),
+                Arguments.of(parameters[7], false),
+                Arguments.of(parameters[8], false)
+        );
     }
 
-    @Test
-    public void testEnvironmentGetNegative() {
-        String script = "some other script";
+
+
+    @DisplayName("Test Environment Get")
+    @ParameterizedTest(name = "Test {index} with script: {arguments}")
+    @MethodSource("provideGetParameters")
+    public void testEnvironmentGet(String script, boolean expected) {
         Matcher matcher = Patterns.ENVIRONMENT_GET.matcher(script);
-        assertFalse(matcher.find());
+        assertEquals(expected, matcher.find());
+    }
+
+    /**
+     * Method to provide the parameters for the testEnvironmentGet method
+     * @return
+     */
+    private static Stream<Arguments> provideGetParameters() {
+        return Stream.of(
+                Arguments.of(parameters[0], false),
+                Arguments.of(parameters[1], true),
+                Arguments.of(parameters[2], false),
+                Arguments.of(parameters[3], false),
+                Arguments.of(parameters[4], false),
+                Arguments.of(parameters[5], false),
+                Arguments.of(parameters[6], false),
+                Arguments.of(parameters[7], false),
+                Arguments.of(parameters[8], false)
+        );
     }
 }
