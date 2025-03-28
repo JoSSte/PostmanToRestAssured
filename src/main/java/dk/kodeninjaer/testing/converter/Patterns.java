9ef9f8912e409539c6e55505a9cb3a0e1e961143
Jonas Stumph Stevnsvig <jonas@stevnsvig.com>
2025-03-28 22:51:01 +0100

    added patterns and tests for them

--- src/main/java/dk/kodeninjaer/testing/converter/Patterns.java
+++ src/main/java/dk/kodeninjaer/testing/converter/Patterns.java
@@ -10,11 +10,24 @@ public final class Patterns {
     private Patterns() {
     }
 
-    public static final Pattern VARIABLE            = Pattern.compile("\\{\\{[a-zA-Z0-9]*\\}\\}");
-    public static final Pattern ENVIRONMENT_SET     = Pattern.compile("pm\\.environment\\.set\\([\"'](.*?)[\"'],\\s*(.*?)\\)");
-    public static final Pattern ENVIRONMENT_GET     = Pattern.compile("pm\\.environment\\.get\\([\"'](.*?)[\"']\\)");
-    public static final Pattern EXPECT              = Pattern.compile("pm\\.expect\\((.*?)\\)\\.to\\.(.*?)\\((.*?)\\)|pm\\.response\\.to\\.have\\.status\\((\\d+)\\)");
-    public static final Pattern TEST                = Pattern.compile("pm\\.test\\([\"'](.*?)[\"'],\\s*function\\s*\\(\\)\\s*\\{\\s*(.*?)\\s*\\}\\)");
+    /**
+     * For matching double-moustache notation variables
+     */
+    public static final Pattern VARIABLE             = Pattern.compile("\\{\\{[a-zA-Z0-9]*\\}\\}");
+    /**
+     * For matching any variable getter in a script 
+     * pm.<scope>.get("variableName")
+     */
+    public static final Pattern VAR_GET              = Pattern.compile("pm\\.(environment|globals|collectionVariables)\\.get\\([\"'](.*?)[\"']\\)");
+    /**
+     * For matching any variable setter in a script 
+     */
+    public static final Pattern VAR_SET              = Pattern.compile("pm\\.(environment|globals|collectionVariables)\\.set\\([\"'](.*?)[\"'],\\s*(.*?)\\)");
+    public static final Pattern ENVIRONMENT_SET      = Pattern.compile("pm\\.environment\\.set\\([\"'](.*?)[\"'],\\s*(.*?)\\)");
+    public static final Pattern ENVIRONMENT_GET      = Pattern.compile("pm\\.environment\\.get\\([\"'](.*?)[\"']\\)");
+    public static final Pattern ENVIRONMENT_GET_PART = Pattern.compile("pm\\.environment\\.get\\([\"'](.*?)[\"']");
+    public static final Pattern EXPECT               = Pattern.compile("pm\\.expect\\((.*?)\\)\\.to\\.(.*?)\\((.*?)\\)|pm\\.response\\.to\\.have\\.status\\((\\d+)\\)");
+    public static final Pattern TEST                 = Pattern.compile("pm\\.test\\([\"'](.*?)[\"'],\\s*function\\s*\\(\\)\\s*\\{\\s*(.*?)\\s*\\}\\)");
     
 
 }
--- src/test/java/dk/kodeninjaer/testing/converter/PatternsTest.java
+++ src/test/java/dk/kodeninjaer/testing/converter/PatternsTest.java
@@ -24,7 +24,14 @@ public class PatternsTest {
         "alert(\"someValue\");",
         "pm.globals.set(\"someKey\",\"someValue\");",
         "pm.globals.get(\"someValue\");",
-        "pm.globals.set(\"someKey\",someReturnValue.method());"
+        "pm.globals.set(\"someKey\",someReturnValue.method());",
+        "pm.environment.get(\"somePartValue\"",
+        "pm.environment.get(\"somePartValue\"XXX",
+        "pm.environment.get(\"somePartValue\"\"",
+        "pm.collectionVariables.set(\"somevarKey\",someReturnValue.method());",
+        "pm.collectionVariables.get(\"somevarKey\");",
+        "pm.collectionVariables.set(\"someKey\",someReturnValue);",
+        "pm.collectionVariables.get(\"someKey\");"
     };
     
 
@@ -37,8 +44,7 @@ public class PatternsTest {
         });
     }
     
-    @DisplayName("Test Environment Set")
-    @ParameterizedTest(name = "Test {index} with script: {arguments}")
+    @ParameterizedTest(name = "Test Environment Set Matching #{index} with script: {arguments}")
     @MethodSource("provideSetParameters")
     public void testEnvironmentSet(String script, boolean expected) {
         Matcher matcher = Patterns.ENVIRONMENT_SET.matcher(script);
@@ -59,14 +65,16 @@ public class PatternsTest {
                 Arguments.of(parameters[5], false),
                 Arguments.of(parameters[6], false),
                 Arguments.of(parameters[7], false),
-                Arguments.of(parameters[8], false)
+                Arguments.of(parameters[8], false),
+                Arguments.of(parameters[9], false),
+                Arguments.of(parameters[10], false),
+                Arguments.of(parameters[11], false)
         );
     }
 
 
 
-    @DisplayName("Test Environment Get")
-    @ParameterizedTest(name = "Test {index} with script: {arguments}")
+    @ParameterizedTest(name = "Test Environment Get #{index} with script: {arguments}")
     @MethodSource("provideGetParameters")
     public void testEnvironmentGet(String script, boolean expected) {
         Matcher matcher = Patterns.ENVIRONMENT_GET.matcher(script);
@@ -87,7 +95,103 @@ public class PatternsTest {
                 Arguments.of(parameters[5], false),
                 Arguments.of(parameters[6], false),
                 Arguments.of(parameters[7], false),
-                Arguments.of(parameters[8], false)
+                Arguments.of(parameters[8], false),
+                Arguments.of(parameters[9], false),
+                Arguments.of(parameters[10], false),
+                Arguments.of(parameters[11], false)
+        );
+    }
+
+    @DisplayName("Test Environment Get")
+    @ParameterizedTest(name = "Test {index} with script: {arguments}")
+    @MethodSource("provideGetPartParameters")
+    public void testEnvironmentGetPartial(String script, boolean expected) {
+        Matcher matcher = Patterns.ENVIRONMENT_GET_PART.matcher(script);
+        assertEquals(expected, matcher.find());
+    }
+
+    /**
+     * Method to provide the parameters for the testEnvironmentGet method
+     * @return
+     */
+    private static Stream<Arguments> provideGetPartParameters() {
+        return Stream.of(
+                Arguments.of(parameters[0], false),
+                Arguments.of(parameters[1], true),
+                Arguments.of(parameters[2], false),
+                Arguments.of(parameters[3], false),
+                Arguments.of(parameters[4], false),
+                Arguments.of(parameters[5], false),
+                Arguments.of(parameters[6], false),
+                Arguments.of(parameters[7], false),
+                Arguments.of(parameters[8], false),
+                Arguments.of(parameters[9], true),
+                Arguments.of(parameters[10], true),
+                Arguments.of(parameters[11], true)
+        );
+    }
+
+    @ParameterizedTest(name = "Test Generic getter #{index} with script: {arguments}")
+    @MethodSource("provideGetterParameters")
+    public void testEnvironmentGetters(String script, boolean expected) {
+        Matcher matcher = Patterns.VAR_GET.matcher(script);
+        assertEquals(expected, matcher.find());
+    }
+
+    /**
+     * Method to provide the parameters for the testEnvironmentGet method
+     * @return
+     */
+    private static Stream<Arguments> provideGetterParameters() {
+        return Stream.of(
+                Arguments.of(parameters[0], false),
+                Arguments.of(parameters[1], true),
+                Arguments.of(parameters[2], false),
+                Arguments.of(parameters[3], false),
+                Arguments.of(parameters[4], false),
+                Arguments.of(parameters[5], false),
+                Arguments.of(parameters[6], false),
+                Arguments.of(parameters[7], true),
+                Arguments.of(parameters[8], false),
+                Arguments.of(parameters[9], false),
+                Arguments.of(parameters[10], false),
+                Arguments.of(parameters[11], false),
+                Arguments.of(parameters[12], false),
+                Arguments.of(parameters[13], true),
+                Arguments.of(parameters[14], false),
+                Arguments.of(parameters[15], true)
+        );
+    }
+
+    @ParameterizedTest(name = "Test Generic setter #{index} with script: {arguments}")
+    @MethodSource("provideSetterParameters")
+    public void testEnvironmentSetters(String script, boolean expected) {
+        Matcher matcher = Patterns.VAR_SET.matcher(script);
+        assertEquals(expected, matcher.find());
+    }
+
+    /**
+     * Method to provide the parameters for the testEnvironmentGet method
+     * @return
+     */
+    private static Stream<Arguments> provideSetterParameters() {
+        return Stream.of(
+                Arguments.of(parameters[0], false),
+                Arguments.of(parameters[1], false),
+                Arguments.of(parameters[2], true),
+                Arguments.of(parameters[3], true),
+                Arguments.of(parameters[4], false),
+                Arguments.of(parameters[5], false),
+                Arguments.of(parameters[6], true),
+                Arguments.of(parameters[7], false),
+                Arguments.of(parameters[8], true),
+                Arguments.of(parameters[9], false),
+                Arguments.of(parameters[10], false),
+                Arguments.of(parameters[11], false),
+                Arguments.of(parameters[12], true),
+                Arguments.of(parameters[13], false),
+                Arguments.of(parameters[14], true),
+                Arguments.of(parameters[15], false)
         );
     }
 }
